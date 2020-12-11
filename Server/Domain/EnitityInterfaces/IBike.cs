c7369ce3e9c5182e689262177e758511aec34f64
Vladlen <vladlen.f@outlook.com>
2020-12-11 18:10:37 +0200

    [trans]: added entity interface 'IBike', entity 'Bike', enums 'Status' and 'Type'

---
+++ Server/Domain/EnitityInterfaces/IBike.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Enums;
+
+namespace Domain.EnitityInterfaces
+{
+    public interface IBike
+    {
+        int Id { get; set; }
+        string Title { get; set; }
+        Type Type { get; set; }
+        decimal Price { get; set; }
+        Status Status { get; set; }
+    }
+}
---
+++ Server/Domain/Entities/Bike.cs
@@ -0,0 +1,16 @@
+﻿using Domain.EnitityInterfaces;
+using Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Models
+{
+    public class Bike : IBike
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Type Type { get; set; }
+        public decimal Price { get; set; }
+        public Status Status { get; set; }
+    }
+}
