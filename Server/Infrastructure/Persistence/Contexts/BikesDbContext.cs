ba82d57ceafc0e977d85422f140badde8ac7a996
Vladlen <vladlen.f@outlook.com>
2020-12-11 21:18:14 +0200

    [trans]: added 'IBikeRepository' repository interface and 'BikeRepository' class

--- Server/Infrastructure/Persistence/Contexts/BikesDbContext.cs
+++ Server/Infrastructure/Persistence/Contexts/BikesDbContext.cs
@@ -1,12 +1,15 @@
 ﻿using Persistence.Configurations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Domain.Entities;
 
 namespace Persistence.Contexts
 {
     public class BikesDbContext : DbContext
     {
         private readonly BikesDbConfiguration _configuration;
+
+        public DbSet<Bike> Bikes { get; set; }
 
         public BikesDbContext()
         {
---
+++ Server/Infrastructure/Persistence/RepositoryInterfaces/IBikeRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Persistence.RepositoryInterfaces
+{
+    public interface IBikeRepository
+    {
+        Task<List<Bike>> GetBikesAsync();
+        Task<Bike> GetBikeByIdAsync(int bikeId);
+        Task<Bike> CreateBikeAsync(Bike bike);
+        Task<Bike> UpdateBikeAsync(Bike bike);
+        Task<Bike> DeleteBikeByIdAsync(int bikeId);
+    }
+}
