fa2df1486c781327979250796c27404d7c489f3d
Vladlen <vladlen.f@outlook.com>
2020-12-13 17:13:03 +0200

    [build]: set up routing & project styles, created 'bike.service' and added project icons

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RentComponent } from './bike/rent/rent.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'rent' },
+  { path: 'rent', component : RentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RentComponent } from './bike/rent/rent.component';
+import { FormsModule } from '@angular/forms';
+import { BikeService } from './shared/bike.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BikeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
